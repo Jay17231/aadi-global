a0383cb592361fd6f94bf4e02788caca859d46d6
Jay17231 <gajjarjay23@gmail.com>
2017-11-25 06:32:07 +0530

    Initial

---
+++ pricing.php
@@ -0,0 +1,162 @@
+
+
+<div class="wrapper">
+	
+	<?php include("header.php"); ?>
+	
+		<div class="why-choose">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-4 col-sm-6 services-solutions">
+						<div class="wc-item text-center">
+							<div class="wc-inner">
+								<img src="assets/images/pills.png" alt="Flower">
+							</div>
+							
+							<div class="overlay">
+								<p>Export</p>
+								<p><i class="fa fa-paper-plane"></i></p>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6">
+						<div class="wc-item text-center">
+							<div class="wc-inner">
+								<img class="center-block" src="assets/images/test-tubes.jpg" alt=""/>
+							</div>
+							
+							<div class="overlay">
+								<p>Import</p>	
+								<p><i class="fa fa-download"></i></p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 col-sm-6">
+						<div class="wc-item text-center">
+							<div class="wc-inner">
+								<img class="center-block" src="assets/images/test-tubes-2.jpg" alt=""/>
+								<img class="center-block" src="assets/images/layer_1692.png" alt=""/>
+							</div>
+							
+							<div class="overlay">
+								<p>Marketing</p>
+								<p><i class="fa fa-television"></i></p>
+							</div>
+						</div>
+					</div>				
+					
+					<div class="col-md-4 col-sm-6">
+						<div class="wc-item text-center">
+							<div class="wc-inner">
+								<img class="center-block" src="assets/images/somerset3.jpg" alt=""/>
+								<img class="center-block" src="assets/images/layer_1679.png" alt=""/>
+							</div>
+							
+							<div class="overlay">
+								<p>Distribution</p>
+								<p><i class="fa fa-credit-card-alt"></i></p>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6">
+						<div class="wc-item text-center meet-leaders">
+							<div class="wc-inner">
+								<p>Our Services</p>
+								<img class="center-block" src="assets/images/layer_1694.png" alt=""/>
+							</div>
+							
+							<div class="overlay">
+								<img class="center-block" src="assets/images/our-services.png" alt=""/>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 col-sm-6">
+						<div class="wc-item text-center">
+							<div class="wc-inner">
+								<img class="center-block" src="assets/images/ocean1.jpg" alt=""/>
+								<img class="center-block" src="assets/images/layer_1696.png" alt=""/>
+							</div>
+							
+							<div class="overlay">
+								<p>Manufacturing</p>
+								<p><i class="fa fa-industry"></i></p>
+							</div>
+						</div>
+					</div>				
+				</div>
+		
+				<div class="clearfix"></div>
+			</div>
+	</div>
+		<div class="spacer"></div>
+	
+	<div class="divider"></div>
+	
+	<div class="spacer"></div>
+	
+		<div class="benefits">
+		<div class="container">
+			<h2>Take a look at our <strong>Global Services </strong></h2>
+			
+			<!-- RPO Services -->
+			<div class="row center-flex">
+				<div class="col-sm-8 col-sm-push-4">
+					<img class="img-responsive img-global col-md-offset-6 img-left" src="assets/images/our-services.png" width="" alt="Image RPO Services">
+				</div>
+				
+				<div class="col-sm-4 col-sm-pull-8 fly-in-left">
+					<div class="benefit-item">
+						<h4>Our Services Include</h4>
+						<p>
+							<ul>
+								<li>Market Research and Intelligence</li>
+								<li>Providing technical support to customers</li>
+								<li>Negotiating transactions</li>
+								<li>Assisting in completion of procedural formalities</li>
+								<li>Providing of logistic support</li>
+								<li>Commitment to resolve quality or delivery related problems</li>
+								<li>Negotiating amicable settlement of differences</li>
+							</ul>
+						</p>
+						<p>We believe in <span class="color-red">total commitment towards business and operations</span>. We commit ourselves to be a reliable Partner, and offer a wide array of services to both customers and suppliers.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="spacer"></div>
+			<div class="divider"></div>
+			<div class="spacer"></div>
+			
+			<!-- Temporary Staffing Services -->
+			<div class="row center-flex">
+			
+				<div class="col-sm-8">
+					<img class="img-responsive img-global" src="assets/images/enviromental-safety.jpg" alt="Image Temporary Staffing">
+				</div>
+				<div class="col-sm-4 fly-in-right">
+					<div class="benefit-item">
+						<h4>Environmental Safety</h4>
+						<p>This is one aspect that is always been on the <span class="color-red">top of our priority list</span>. All our processes conform to the international standards as in accord to the environment laws. Keeping this very important environment friendly attitude and human welfare into consideration, we have our own process comprising of primary, secondary and tertiary treatments. All liquid and solid wastes are <span class="color-red">disposed off in an environmentally and legally acceptable manner</span>.</p>
+						<a href="features.php" class="btn-md">Take a look</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="spacer"></div>
+			<div class="spacer"></div>						
+		</div>
+	</div>
+	
+	<div class="divider"></div>
+	
+	<div class="spacer"></div>
+	
+	<?php include ("footer.php");?>
+
+
+</div>
